8dbeb39ba84f480f8cc7265729f45393af036810
Hunter Schafer <hschafer@uw.edu>
2020-07-03 16:07:43 -0700

    Update navigation and home page

--- website/components/chapter.js
+++ website/components/chapter.js
@@ -1,10 +1,11 @@
-import Head from 'next/head'
+import Link from 'next/link'
 import styles from './chapter.module.css'
 
 export default function Chapter({ children, chapterNumber, title}) {
     return <>
         <main>
             <article>
+            <p><Link href="/"><a>&#8592; Back Home</a></Link></p>
             <h1>Chapter {chapterNumber}: {title}</h1>
             {children}
             </article>    
