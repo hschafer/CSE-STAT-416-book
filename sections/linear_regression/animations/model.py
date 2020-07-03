fe4c4dee2b5c4f0a8386f58c1bc9c806024a8cc7
Hunter Schafer <hschafer@uw.edu>
2020-07-03 16:05:52 -0700

    Clean up linear regression animation

--- sections/linear_regression/animations/model.py
+++ sections/linear_regression/animations/model.py
@@ -4,8 +4,10 @@ from manimlib.imports import *
 
 class Model(Scene):
     def construct(self):
+
+        # True function
         def f(x):
-            # Weird hacks so manim gives inputs of 0 to 4, but we want 0 to 1
+            # Weird hacks so manim gives inputs of 0 to 4, but we want 0 to 1 for formula
             x = x / 4
             return 21 * x ** 3 - 37 * x ** 2 + 20 * x
 
@@ -20,6 +22,7 @@ class Model(Scene):
                        3.48042945, 2.81497412, 2.11811096, 3.47236632, 3.05471791,
                        3.37113595, 2.48020868, 3.33907243, 3.05561223, 2.30361556])
 
+        # Generate the axes
         axes = Axes(x_min=0, x_max=4,
                     y_min=0, y_max=4,
                     # center_point=[-1, -1, 0],
@@ -28,25 +31,21 @@ class Model(Scene):
                         'include_ticks': False
                     })
         axes.move_to([-1, -1, 0])
-        # axes = axes.shift_onto_screen()
         self.play(ShowCreation(axes, run_time=2, lag_ratio=0.1))
 
         # Create text
         data_text = TexMobject(r'\{(x_i, y_i)\}_{i=1}^n')
         function_text = TextMobject(r'True function: {$f($}{$x$}{$)$}',
                                     tex_to_color_map={'{$f($}': GREEN, '{$)$}': GREEN})
-        print(function_text.tex_strings)
         text_group = VGroup(data_text, function_text).arrange(DOWN)
         text_group.to_corner(UP + LEFT)
 
         # Draw points
         self.play(Write(data_text))
-
         for x, y in zip(xs, ys):
             point = axes.coords_to_point(x, y, 0)
             dot = Dot(point)
             self.play(FadeIn(dot), run_time=0.1)
-
         self.wait()
 
         # Draw Function
@@ -68,13 +67,13 @@ class Model(Scene):
         model_text.to_corner(UP + RIGHT)
         self.play(Write(model_text))
 
-
-        # Highlight one point
+        # Pick one point to highlight
         index = 19
         origin = axes.coords_to_point(0, 0, 0)
         point = axes.coords_to_point(xs[index], ys[index], 0)
         expected_value = axes.coords_to_point(xs[index], f(xs[index]), 0)
 
+        # Draw a vertical line from its x to the expected value
         dot = Dot(expected_value, color=GREEN)
         x_line = DashedLine(start=[point[0], origin[1], 0],
                             end=expected_value)
@@ -85,9 +84,9 @@ class Model(Scene):
             AnimationGroup(FadeIn(x_text), FadeIn(x_line, duration=1)),
             FadeIn(dot),
             lag_ratio=0.1)
-
         self.wait()
 
+        # Draw the epislon from the expected to observed
         epsilon = DashedLine(start=expected_value,
                              end=point,
                              color=YELLOW)
@@ -95,74 +94,13 @@ class Model(Scene):
         epsilon_text.scale(0.7)
         epsilon_text.next_to(epsilon, direction=LEFT*0.4)
         self.play(ShowCreation(epsilon, duration=1), Write(epsilon_text))
-
         self.wait()
 
+        # Draw a line to show how it computes the y from that
         y_line = DashedLine(start=point,
                             end=[origin[0], point[1], 0],
                             color=BLUE)
         y_text = TexMobject(r'y_7', color=BLUE)
         y_text.next_to(y_line, direction=LEFT)
         self.play(FadeIn(y_line, duration=2), Write(y_text), lag_ratio=0.5)
-
         self.wait(duration=4)
-
-
-
-
-class OpeningManimExample(Scene):
-    def construct(self):
-        title = TextMobject("This is some \\LaTeX")
-        basel = TexMobject(
-            "\\sum_{n=1}^\\infty "
-            "\\frac{1}{n^2} = \\frac{\\pi^2}{6}"
-        )
-        VGroup(title, basel).arrange(DOWN)
-        self.play(
-            Write(title),
-            FadeInFrom(basel, UP),
-        )
-        self.wait()
-
-        transform_title = TextMobject("That was a transform")
-        transform_title.to_corner(UP + LEFT)
-        self.play(
-            Transform(title, transform_title),
-            LaggedStart(*map(FadeOutAndShiftDown, basel)),
-        )
-        self.wait()
-
-        grid = NumberPlane()
-        grid_title = TextMobject("This is a grid")
-        grid_title.scale(1.5)
-        grid_title.move_to(transform_title)
-
-        self.add(grid, grid_title)  # Make sure title is on top of grid
-        self.play(
-            FadeOut(title),
-            FadeInFromDown(grid_title),
-            ShowCreation(grid, run_time=3, lag_ratio=0.1),
-        )
-        self.wait()
-
-        grid_transform_title = TextMobject(
-            "That was a non-linear function \\\\"
-            "applied to the grid"
-        )
-        grid_transform_title.move_to(grid_title, UL)
-        grid.prepare_for_nonlinear_transform()
-        self.play(
-            grid.apply_function,
-            lambda p: p + np.array([
-                np.sin(p[1]),
-                np.sin(p[0]),
-                0,
-            ]),
-            run_time=3,
-        )
-        self.wait()
-        self.play(
-            Transform(grid_title, grid_transform_title)
-        )
-        self.wait()
-
