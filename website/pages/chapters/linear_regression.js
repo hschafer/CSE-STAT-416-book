968caf5725a09eb057f9be5a7870f7c23d3e852b
Hunter Schafer <hschafer@uw.edu>
2020-07-13 17:55:58 -0700

    Update draft of linear regression text

--- website/pages/chapters/linear_regression.js
+++ website/pages/chapters/linear_regression.js
@@ -115,9 +115,11 @@ export default function LinearRegression() {
 
           <p>
             To be a bit more precise, we will specify how we believe
-            "approximately equal" works for this scenario. In this example, we
-            will use a very common assumption about how the input and target
-            relate. We first show the formula, and then explain the parts.{" "}
+            "approximately equal" works for this scenario. Another term for "the
+            assumptions we make" is the <b>model</b> we are using. In this
+            example, we will use a very common model about how the input and
+            target relate. We first show the formula, and then explain the
+            parts.{" "}
             <MarginNote id="subscript-notation">
               📝 <em>Notation:</em> When we subscript a variable like{" "}
               <IM math={`x_i`} />, it means we are talking about the{" "}
@@ -132,7 +134,7 @@ export default function LinearRegression() {
           </p>
           <BM
             math={`
-                y_i = f(x_i) + \\epsilon_i
+                y_i = f(x_i) + \\varepsilon_i
             `}
           />
 
@@ -141,8 +143,22 @@ export default function LinearRegression() {
             saw, <IM math={`y_i`} />, come from the true function{" "}
             <IM math={`f`} /> being applied to the input data{" "}
             <IM math={`x_i`} />, but that there was some noise{" "}
-            <IM math={`\\epsilon_i`} /> added to it that may cause some error.
-            The animation below shows a visual representation of this model.
+            <IM math={`\\varepsilon_i`} /> added to it that may cause some
+            error. We also will make an assumption about how the{" "}
+            <IM math={`\\varepsilon_i`} /> as well: we assume that{" "}
+            <IM math={`\\mathbb{E}\\left[\\varepsilon_i\\right] = 0`} />, which
+            means on average we expect the noise to average out to 0 (i.e., it's
+            not biased to be positive or negative). The animation below shows a
+            visual representation of this model.
+            <MarginNote id="expectation">
+              📝 <em>Notation:</em> <IM math={`\\mathbb{E}\\left[X\\right]`} />{" "}
+              is the expected value of a random variable (the "average"
+              outcome). See more{" "}
+              <a href="https://www.investopedia.com/terms/e/expected-value.asp">
+                here
+              </a>
+              .
+            </MarginNote>
           </p>
 
           <video autoPlay controls loop>
@@ -163,6 +179,101 @@ export default function LinearRegression() {
             (noisy) data of inputs/outputs and will have to somehow try to
             approximate <IM math={"f"} /> from just the given data.
           </p>
+
+          <p>
+            To phrase this challenge mathematically, a common goal in machine
+            learning is to learn a function <IM math={`\\hat{f}`} />{" "}
+            <MarginNote id="hat">
+              📝 <em>Notation:</em> A <IM math={`\\hat{\\ }`} /> in math
+              notation almost always means "estimate". In other words,{" "}
+              <IM math={`\\hat{f}`} /> is our best estimate of this unknown
+              function <IM math={`f`} />.
+            </MarginNote>
+            from the data that approximates <IM math={`f`} /> as best as we can.
+            We can then use this <IM math={`\\hat{f}`} /> to make predictions
+            about new data by evaluating <IM math={`\\hat{y} = \\hat{f}(x)`} />.
+            It's likely our estimate won't be exactly correct, but our hope is
+            to get one that is as close to this unknown truth as possible. More
+            on how to learn this approximation later, but it has something to do
+            with finding a function that closely matches the data were given.
+          </p>
+
+          <p>
+            TODO Animation of slide 24
+            https://courses.cs.washington.edu/courses/cse416/19su/files/lectures/lec1/1_regression.pdf
+          </p>
+        </section>
+
+        <section>
+          <h3>ML Pipeline</h3>
+
+          <p>
+            Machine learning is a broad field with many algorithms and
+            techniques that solve different parts of a learning task. We provide{" "}
+            a generic framework of most machine learning systems that we call
+            the <b>ML Pipeline</b>, which is shown in the image below. Whenever
+            we are learning a new topic, try to identify which part of the
+            pipeline it works in.
+          </p>
+          <p>
+            TODO ML Pipeline image. TODO add step after hat_y to evaluate the
+            model.
+          </p>
+          <p>
+            We will talk about each component in detail for our first machine
+            learning model, linear regression, but we first provide a broad
+            overview of what they signify.
+            <MarginNote id="notation">
+              📝 <em>Notation:</em> Common notation around this pipeline.
+              <ul>
+                <li>
+                  <IM math={`x`} /> The input feature(s) for an example.
+                </li>
+                <li>
+                  <IM math={`y`} /> The outcome/target for an example.
+                </li>
+                <li>
+                  <IM math={`\\hat{f}`} /> The learned predictor.
+                </li>
+                <li>
+                  <IM math={`\\hat{y}`} /> The prediction using the learned
+                  predictor. Commonly <IM math={`\\hat{y} = \\hat{f}(x)`} />.
+                </li>
+              </ul>
+            </MarginNote>
+          </p>
+
+          <ul>
+            <li>
+              Training Data: The dataset we were given (e.g., sq. ft., price
+              pairs).
+            </li>
+            <li>
+              Feature Extraction: Any transformations we do to the raw training
+              data.
+            </li>
+            <li>ML Model: The process we believe how the world works.</li>
+            <li>
+              Quality Metric: Some sort of calculation to identify how good a
+              candidate predictor is. This is
+            </li>
+            <li>
+              ML Algorithm: An optimization procedure that uses the Quality
+              Metric to select the "best" predictor.
+            </li>
+          </ul>
+
+          {/*
+            Potential Concepts:
+              * Input vs output (/ other terms)
+              * Model
+              * Difference between f and \hat{f}
+
+            Concept Questions:
+              * Suppose the process for generating the \varepsilon_is was flipping a coin and if its heads add 1 and add 0 if tails.
+                Does this scenario fall under our assumed model?
+              *
+          */}
         </section>
       </Chapter>
     </>
