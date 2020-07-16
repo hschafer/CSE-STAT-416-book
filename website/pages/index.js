35e62475259d197f317ee019a46bbeb5a6f29f77
Hunter Schafer <hschafer@uw.edu>
2020-07-16 11:55:45 -0700

    Update index to have acknowledgements

--- website/pages/index.js
+++ website/pages/index.js
@@ -15,22 +15,70 @@ export default function Home() {
 
       <main>
         <h1 className="title">CSE/STAT 416 Book</h1>
-
-        <p className="description">
-          This book is designed to teach machine learning to a wide audience.
+        <p className="subtitle">
+          Author:{" "}
+          <a href="https://homes.cs.washington.edu/~hschafer/">
+            Hunter Schafer
+          </a>
         </p>
 
-        <h2>Table of Contents</h2>
+        <section>
+          <p>
+            This book is designed for the CSE/STAT 416 course at the University
+            of Washington. The course is designed to teach machine learning to a
+            wide audience. We assume comparatively less mathematical background
+            than other ML books to make the content more accessible to more
+            students. Our mission is{" "}
+            <b>to enable anyone who is interested to learn machine learning</b>{" "}
+            which means our job is{" "}
+            <b>to make tough concepts intuitive and applicable</b>. Read more in
+            the introduction.
+          </p>
 
-        <ol>
-          {tableOfContents.map((chapter) => (
-            <li key={chapter.file} value={chapter.chapterNumber}>
-              <Link href={`/chapters/${chapter.file}`}>
-                <a>{chapter.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ol>
+          <p>
+            This books is very much a work in progress. We are always interested
+            in hearing your feedback so please let us know what you think with
+            one of the feedback avenues below.
+          </p>
+
+          <h2>Table of Contents</h2>
+
+          <ol>
+            {tableOfContents.map((chapter) => (
+              <li key={chapter.file} value={chapter.chapterNumber}>
+                <Link href={`/chapters/${chapter.file}`}>
+                  <a>{chapter.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ol>
+
+          <h2>Acknowledgements</h2>
+
+          <p>
+            CSE/STAT 416 was originally developed at UW by{" "}
+            <a href="https://homes.cs.washington.edu/~ebfox/">Emily Fox</a>{" "}
+            based on the Coursera course created by her and{" "}
+            <a href="https://homes.cs.washington.edu/~guestrin/">
+              Carlos Guestrin
+            </a>
+            . Hunter was a major contributor to the adaption of the online class
+            to its first offering at UW, and has recently been the caretaker of
+            the course as it continues to evolve. While the format, text, and
+            animations of the book are Hunter's creations, this book wouldn't
+            exist without Emily and Carlos' hard work in building a story for
+            teaching ML to a broader audience.
+          </p>
+
+          <p>
+            All of the code for this book (including the animations) can be
+            found on our{" "}
+            <a href="https://github.com/hschafer/CSE-STAT-416-book">
+              GitHub repository
+            </a>
+            .
+          </p>
+        </section>
       </main>
     </Layout>
   );
