f3ec64d49feae39b208b7e9c5175053b40597b55
Hunter Schafer <hschafer@uw.edu>
2020-07-16 21:20:14 -0700

    Fix typos and grammatical mistakes

--- website/pages/chapters/introduction.js
+++ website/pages/chapters/introduction.js
@@ -10,14 +10,14 @@ export default function Introduction() {
         <section>
           <p>
             At this point, we hardly have to introduce the term{" "}
-            <b>"machine learning"</b> (ML) as it likely to be something you have
-            heard at least in passing. There are near daily mentions of "machine
-            learning systems" or "deep learning systems" in the news we read and
-            the media we consume. Machine learning is changing the world. It is
-            affecting aspects of our lives we couldn't even imagine 5 or 10
-            years ago, and progress in the field is only growing more rapidly.
-            As an example, we showcase a very small number of examples, from the
-            very fun to truly terrifying:
+            <b>"machine learning"</b> (ML) as it's likely to be something you
+            have heard at least in passing. There are near daily mentions of
+            "machine learning systems" or "deep learning systems" in the news we
+            read and the media we consume. Machine learning is changing the
+            world. It is affecting aspects of our lives we couldn't even imagine
+            5 or 10 years ago, and progress in the field is only growing more
+            rapidly. As an example, we showcase a very small number of ways ML
+            can be applied to our lives, from the very fun to truly terrifying:
           </p>
           <ul>
             <li>
@@ -44,26 +44,25 @@ export default function Introduction() {
             reading this book, but the concepts and applications you will learn
             here will enable you to make intelligent decisions whenever you
             interact with these systems. Our hope is that we can show you the
-            awesome potential of these systems and how you can build them. At
-            the same time, we want you to be considering the serious
-            ramifications of automation that afford and how to avoid some
-            dangerous pitfalls that can harm people.
+            awesome potential of these systems and how you to build them well.
+            At the same time, we want you to be considering the serious
+            ramifications of automation that these models afford and how to
+            avoid some dangerous pitfalls that can end up harming people.
           </p>
 
           <p>
             The remaining part of this chapter is broken into 3 parts. Before we
             give a broad overview of what ML is and the things you will see in
-            this book, we we want to establish some of the values we are
-            bringing in as authors of this book and how to effectively learn
-            from this book.
+            this book at the end of this chapter, we we want to establish some
+            of the values we are bringing in as authors of this book and how to
+            effectively learn from this book.
           </p>
 
           <h2>Our Values</h2>
           <p>
             Our values determine what we prioritize in structure and content.
-            This is not to say these are the only things that should be valued
-            or that we value in general, but these our the ones we use as a
-            guiding principle for this book.
+            This is not to say these are the only things that should be valued,
+            but these our the ones we use as a guiding principle for this book.
           </p>
 
           <ul>
@@ -75,7 +74,9 @@ export default function Introduction() {
                 </b>
                 We've already highlighted why we think everyone learning this is
                 important, but wanted to write it down as a core value.
-                Importantly, we used a pretty vague word there "understand". To
+              </p>
+              <p>
+                Importantly, we used a pretty vague word there: "understand". To
                 understand something can mean different things to different
                 people depending on their goals and where they are in their
                 educational journey. We can't determine the things you will get
@@ -110,11 +111,14 @@ export default function Introduction() {
               </p>
               <p>
                 This does not mean we won't use math at all! While we try to
-                minimize pre-requisite knowledge, we do rely on a base-level
-                understanding of probability and a willingness to learn new
-                notation and concepts. Mathematical notation can be very
-                effective for communication and we use it frequently to
-                communicate more precisely/effectively.
+                minimize pre-requisite knowledge, we still require some.
+                <MarginNote id="prereqs">
+                  We do rely on a base-level understanding of probability and a
+                  willingness to learn new notation and concepts
+                </MarginNote>{" "}
+                Mathematical notation can be very effective for communication
+                and we use it frequently to communicate more
+                precisely/effectively.
               </p>
             </li>
           </ul>
@@ -153,7 +157,7 @@ export default function Introduction() {
           <h2>How Learning Works</h2>
           <p>
             Learning is a complex process that whole fields of research focus on
-            learning (ha) how it works. The research of effective learning was
+            learning (ha!) how it works. The research of effective learning was
             summarized very well in{" "}
             <a href="https://www.retrievalpractice.org/make-it-stick">
               "Make it Stick" by Peter C. Brown, Henry L. Roediger III, and Mark
@@ -191,9 +195,9 @@ export default function Introduction() {
             easy, that might be a good sign it's not effective. In "Make it
             Stick", they argue the most important thing to learning effectively
             is being an active participant in the process. When you are taking
-            charge of your learning, you will face many setbacks, but you
-            shouldn't perceive those as failures, but instead as areas to grow.
-            Active learning involves three key steps:
+            charge of your learning, you will face many setbacks; you shouldn't
+            perceive those as failures, but instead as areas to grow. "Make it
+            Stick" argues that active learning involves three key steps:
           </p>
 
           <ul>
@@ -232,9 +236,9 @@ export default function Introduction() {
               you should mix it up and test them together. Retrieving knowledge
               in real life rarely has a context like "I'm studying for Chapter 1
               right now, so I only need to think of Chapter 1 things". In
-              reality, you will need to practice bringing up any subset of
-              knowledge so your practice should reflect that if you want to see
-              improvements in retrieval{" "}
+              reality, you will need to practice bringing up <em>any</em> subset
+              of knowledge. Therefore, your practice should reflect that goal if
+              you want to see improvements of retrieval in realistic contexts.
               <MarginNote id="baseball">
                 See{" "}
                 <a href="https://pubmed.ncbi.nlm.nih.gov/8084699/">
@@ -243,7 +247,6 @@ export default function Introduction() {
                 about the effects of mixed-practice on professional baseball
                 players.
               </MarginNote>
-              .
             </li>
           </ul>
 
@@ -252,11 +255,14 @@ export default function Introduction() {
             employ these three study tips. You should be building up meaningful
             notes, reflecting what you've learned and relating it to past
             concepts, testing yourself regularly, and spacing-out and mixing-up
-            your practice. We highly recommend (would recommend it if we could)
+            your practice. We highly recommend (would require it if we could)
             using a tool like Anki to help you test yourself on concepts. It's
             rare that we care about a word-for-word definition of a term or
             formula, but instead care about the intuition and understanding that
-            comes from being familiar with that concept.
+            comes from being familiar with that concept. Don't penalize yourself
+            when studying for messing up a single word as long as you can assess
+            that you have understood that concept (don't worry, it will always
+            come back later in Anki).
           </p>
         </section>
 
@@ -281,9 +287,8 @@ export default function Introduction() {
 
           <p>
             This is just trying to state in a very formal manner that machine
-            learning is generally programs that learn from data that improve at
-            some task by learning from data rather than a human hard-coding the
-            rules of the task.{" "}
+            learning is a program that improves at some task by learning from
+            data rather than a human hard-coding the rules of the task.{" "}
             <MarginNote id="ml-steps">
               <img
                 src="/animations/introduction/ml-steps.png"
@@ -293,43 +298,57 @@ export default function Introduction() {
             </MarginNote>
           </p>
           <p>
-            For example, consider the task of trying to make a system that
-            classifies an image as containing a dog or a cat. One approach would
-            be to try to write a program (e.g., a Python program) that looks at
-            the pixels of the picture in a big <code>if/else</code> statement to
-            make the decision. You (nor I) would really have any idea where to
-            start writing such a program.
+            For example, consider the task of building a system that classifies
+            an image as containing a dog or a cat. One approach would be to try
+            to write a program (e.g., a Python program) that looks at the pixels
+            of the picture in a big <code>if/else</code> statement to make the
+            decision. You (nor I) would really have any idea where to start
+            writing such a program.
           </p>
 
           <p>
             Instead, the machine learning approach is to feed a bunch of images
-            that have already been labelled as cat images or dog images as input
-            to some learning algorithm, and letting that algorithm learn the
-            patterns from the data itself. The ultimate goal of such a system is
-            to train the computer to not just be able to answer this dog-vs.-cat
-            question for images it has seen, but also to <em>generalize</em> to
-            any dog or cat picture it might encounter in the future.
+            that have already been labelled as cat images or dog images{" "}
+            <MarginNote id="training-data">
+              We call this the training data.
+            </MarginNote>{" "}
+            as input to some learning algorithm, and letting that algorithm
+            learn the patterns from the data itself. The ultimate goal of such a
+            system is to train the computer to not just be able to answer this
+            dog-vs.-cat question for images it has seen, but also to{" "}
+            <em>generalize</em> to any dog or cat picture it might encounter in
+            the future.
           </p>
 
           <p>
             In the next chapter, we will introduce more concrete concepts that
-            form a machine learning "pipeline", so we wont' dive into these
-            details further here. As our last point-of-order in this chapter, we
-            want to give a very broad overview of how this book is structured.
+            form a machine learning "pipeline", so we won't dive into these
+            details further here.
+          </p>
+          <p>
+            As our last point-of-order in this chapter, we want to give a very
+            broad overview of how this book is structured.
           </p>
 
           <p>
             We will tackle this introduction to machine learning with 5
-            overarching case-studies to guide us. As we mentioned before, the
+            overarching case studies to guide us. As we mentioned before, the
             contexts that these case studies focus on are not the only
             applications that are of import in machine learning. We spend the
-            majority of our focus on these five examples for consistency to make
-            the relationships between concepts more concrete. Each case study
-            will span more than one chapter as indicated in the table of
-            contents to show how the various sub-topics relate. As a reminder
-            from the section on "How Learning Works", it's important for you to
-            be constantly pausing and reflection so that you can test your
-            understanding and connect ideas to other sections.
+            majority of our focus on these five examples for consistency and
+            simplicity.
+            <MarginNote id="case-study">
+              This helps you form connections between these concepts.
+            </MarginNote>
+            Each case study will span more than one chapter as indicated in the
+            table of contents; each chapter corresponds roughly to one lecture
+            of the UW CSE/STAT 416 course.{" "}
+            <MarginNote id="reminder">
+              As a reminder from the section on "How Learning Works", it's
+              important for you to be constantly pausing and reflection so that
+              you can test your understanding and connect ideas to other
+              sections.
+            </MarginNote>
           </p>
 
           <p>The five case studies we will examine in this book are:</p>
