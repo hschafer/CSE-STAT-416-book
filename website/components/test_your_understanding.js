2497ba69cde8903d0b5be297470fc7a79fa7cde6
Hunter Schafer <hschafer@uw.edu>
2020-07-26 14:26:50 -0700

    Update assessing performance card

--- website/components/test_your_understanding.js
+++ website/components/test_your_understanding.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import styles from "./test_your_understanding.module.css";
 
-export default function TYU({ header, children }) {
-  const [expanded, setExpanded] = useState(false);
-  return (
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+/* return (
     <div class={styles.card} onClick={() => setExpanded(!expanded)}>
       <div class={styles.header}>
         <span class={styles.title}>{header}</span>
@@ -14,5 +16,23 @@ export default function TYU({ header, children }) {
         {children}
       </div>
     </div>
+  ); */
+export default function TYU({ header, children }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <Accordion className="main-column">
+      <Card>
+        <Card.Header className={styles.header}>
+          <span className={styles.question_mark}>&#x3f;</span>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            {header}
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>{children}</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   );
 }
