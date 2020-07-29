50647be9b5d5711e49bec43d37eadd4c0b48dbb2
Hunter Schafer <hschafer@uw.edu>
2020-07-29 11:17:47 -0700

    Add basic lunr search for small set of documents

--- website/components/search.js
+++ website/components/search.js
@@ -2,11 +2,68 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
-export default function Search() {
+import lunr from "lunr";
+
+export default function Search({ searchIndex }) {
+  var searchIndex = lunr(function () {
+    this.field("title");
+    this.field("text");
+    this.ref("id");
+
+    var documents = [
+      {
+        title: "Lunr",
+        text: "Like Solr, but much smaller, and not as bright.",
+        id: "1",
+      },
+      {
+        title: "React",
+        text: "A JavaScript library for building user interfaces.",
+        id: "2",
+      },
+      {
+        title: "Lodash",
+        text:
+          "A modern JavaScript utility library delivering modularity, performance & extras.",
+        id: "3",
+      },
+    ];
+    documents.forEach(function (doc) {
+      this.add(doc);
+    }, this);
+  });
+
+  function inputReceived(event) {
+    var query = event.target.value;
+    console.log("Typing", query);
+
+    // From https://github.com/olivernn/lunr.js/issues/287
+    var results = searchIndex.query(function (q) {
+      // exact matches should have the highest boost
+      q.term(query, { boost: 100 });
+
+      // prefix matches should be boosted slightly
+      q.term(query, {
+        boost: 10,
+        usePipeline: false,
+        wildcard: lunr.Query.wildcard.TRAILING,
+      });
+
+      // finally, try a fuzzy search, without any boost
+      q.term(query, { boost: 1, usePipeline: false, editDistance: 1 });
+    });
+    console.log("Result", results);
+  }
+
   return (
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
+      <FormControl
+        name="query"
+        onChange={inputReceived}
+        type="text"
+        placeholder="Search"
+        className="mr-sm-2"
+      />
     </Form>
   );
 }
