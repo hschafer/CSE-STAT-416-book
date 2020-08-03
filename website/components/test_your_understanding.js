4c5e77adcc8612de750b4c28c07a1ef5b2af7e7b
Hunter Schafer <hschafer@uw.edu>
2020-08-02 22:53:03 -0700

    Add intro for ridge

--- website/components/test_your_understanding.js
+++ website/components/test_your_understanding.js
@@ -5,24 +5,16 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import styles from "./test_your_understanding.module.css";
 
-/* return (
-    <div class={styles.card} onClick={() => setExpanded(!expanded)}>
-      <div class={styles.header}>
-        <span class={styles.title}>{header}</span>
-        <span class={styles.arrow}>{expanded ? `\u25BD` : `\u25B7`}</span>
-      </div>
-
-      <div class={styles.body + " " + (expanded ? styles.expanded : "")}>
-        {children}
-      </div>
-    </div>
-  ); */
-
 function TYUHeader({ children }) {
   return (
     <Card.Header className={styles.header}>
       <span className={styles.question_mark}>&#x3f;</span>
-      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+      <Accordion.Toggle
+        className={styles.card_button}
+        as={Button}
+        variant="link"
+        eventKey="0"
+      >
         {children}
       </Accordion.Toggle>
     </Card.Header>
