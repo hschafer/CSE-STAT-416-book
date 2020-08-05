56f98777e129a14162fc521858f5b6de51e2ccbe
Hunter Schafer <hschafer@uw.edu>
2020-08-05 13:48:12 -0700

    Implement context to make the TYU expandable indepently

--- website/components/test_your_understanding.js
+++ website/components/test_your_understanding.js
@@ -6,7 +6,14 @@ import Card from "react-bootstrap/Card";
 import TYUContext from "../contexts/test_your_understanding";
 import styles from "./test_your_understanding.module.css";
 
+// This is the context to shared for this TYU instance
+// Keeping it in this module since it's local to these components
+const ExpandContext = React.createContext();
+
+// Defines the header component of the TYU.
+// This is the box the user can click on that's always visible.
 function TYUHeader({ children }) {
+  var expandContext = useContext(ExpandContext);
   return (
     <Card.Header className={styles.header}>
       <span className={styles.question_mark}>&#x3f;</span>
@@ -15,6 +22,7 @@ function TYUHeader({ children }) {
         as={Button}
         variant="link"
         eventKey="0"
+        onClick={() => expandContext.setExpand(!expandContext.expand)}
       >
         {children}
       </Accordion.Toggle>
@@ -22,6 +30,7 @@ function TYUHeader({ children }) {
   );
 }
 
+// This is the body of the TYU card that is sometimes hidden.
 function TYUExplanation({ children }) {
   return (
     <Accordion.Collapse eventKey="0">
@@ -30,18 +39,35 @@ function TYUExplanation({ children }) {
   );
 }
 
+// Wrapper for TYU. Children should be a TYU.Header and TYU.Explanation
+// Can be expanded by clicking or using the expand all.
 function TYU({ children }) {
-  const context = useContext(TYUContext);
-  var openKey = context.expandAllTYU ? "0" : "";
+  const expandAllContext = useContext(TYUContext);
+  const [expand, setExpand] = useState(expandAllContext.expandAllTYU);
+
+  // Not really proud of this, but to make sure we can override the expand
+  // state, we have to track our previous notion of the context's expandAll
+  // to see if this render is a change to that context's state.
+  const [prevExpandAll, setPrevExpandAll] = useState(
+    expandAllContext.expandAllTYU
+  );
+  if (expandAllContext.expandAllTYU !== prevExpandAll) {
+    setPrevExpandAll(expandAllContext.expandAllTYU);
+    setExpand(expandAllContext.expandAllTYU);
+  }
+
+  var openKey = expand ? "0" : "";
   var activeKey = openKey ? openKey : undefined;
   return (
-    <Accordion
-      defaultActiveKey={openKey}
-      activeKey={activeKey}
-      className={"main-column " + styles.tyuAccordion}
-    >
-      <Card className={styles.tyuCard}>{children}</Card>
-    </Accordion>
+    <ExpandContext.Provider value={{ expand, setExpand }}>
+      <Accordion
+        defaultActiveKey={openKey}
+        activeKey={activeKey}
+        className={"main-column " + styles.tyuAccordion}
+      >
+        <Card className={styles.tyuCard}>{children}</Card>
+      </Accordion>
+    </ExpandContext.Provider>
   );
 }
 
