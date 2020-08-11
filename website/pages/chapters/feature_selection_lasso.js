97dbd1651fe2bf2e5b63dba648e9148e89a052ee
Hunter Schafer <hschafer@uw.edu>
2020-08-11 13:50:06 -0700

    Add outline of LASSO chapter

--- website/pages/chapters/feature_selection_lasso.js
+++ website/pages/chapters/feature_selection_lasso.js
@@ -10,7 +10,53 @@ export default function FeatureSelectionLASSO() {
   return (
     <Chapter fileName="feature_selection_lasso">
       <section>
-        <p>Placeholder</p>
+        <p>Recap</p>
+
+        <h3>
+          Details of selecting <IM math={`\\lambda`} />
+        </h3>
+      </section>
+
+      <section>
+        <h2>Feature selection</h2>
+
+        <h3>Sparsity Examples</h3>
+      </section>
+
+      <section>
+        <h2>Algorithm 1: All Subsets</h2>
+
+        <h3>Selecting best subset</h3>
+
+        <h3>Algorithmic Efficiency</h3>
+
+        <h3>Approximating with greedy algorithm</h3>
+      </section>
+
+      <section>
+        <h2>Algorithm 2: Ridge Regression</h2>
+
+        <h3>Problems with Ridge for feature selection</h3>
+      </section>
+
+      <section>
+        <h2>Algorithm 3: LASSO Regression</h2>
+
+        <h3>Coefficient Paths</h3>
+
+        <h3>Why sparse solutions?</h3>
+
+        <h3>
+          Selecting <IM math={`\\lambda`} />
+        </h3>
+
+        <h3>Practical Considerations</h3>
+
+        <h3>Interpreting results</h3>
+      </section>
+
+      <section>
+        <h2>Recap</h2>
       </section>
     </Chapter>
   );
