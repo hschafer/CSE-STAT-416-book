790c1752180730708ac5ac1c3e0bbcd83973b4d3
Hunter Schafer <hschafer@uw.edu>
2020-08-13 15:29:10 -0700

    Finish recap for start of LASSO chapter

--- website/pages/chapters/feature_selection_lasso.js
+++ website/pages/chapters/feature_selection_lasso.js
@@ -84,6 +84,148 @@ export default function FeatureSelectionLASSO() {
         <h3>
           Details of selecting <IM math={`\\lambda`} />
         </h3>
+
+        <TYU>
+          <TYU.HeaderMC
+            options={[
+              <>
+                Smallest <IM math={`RSS(\\hat{w})`} /> on the{" "}
+                <em>training set</em>.
+              </>,
+              <>
+                Smallest <IM math={`RSS(\\hat{w})`} /> on the <em>test set</em>.
+              </>,
+              <>
+                Smallest <IM math={`RSS(\\hat{w})`} /> on the{" "}
+                <em>validation set</em>.
+              </>,
+              <>
+                Smallest{" "}
+                <IM math={`RSS(\\hat{w}) + \\lambda||\\hat{w}_{1:D}||_2^2`} />{" "}
+                on the <em>training set</em>.
+              </>,
+              <>
+                Smallest{" "}
+                <IM math={`RSS(\\hat{w}) + \\lambda||\\hat{w}_{1:D}||_2^2`} />{" "}
+                on the <em>test set</em>.
+              </>,
+              <>
+                Smallest{" "}
+                <IM math={`RSS(\\hat{w}) + \\lambda||\\hat{w}_{1:D}||_2^2`} />{" "}
+                on the <em>validation set</em>.
+              </>,
+            ]}
+            correct="2"
+            name="selecting-lambda"
+          >
+            How would you go about selecting <IM math={`\\lambda`} /> using our
+            model selection procedure? You would choose the{" "}
+            <IM math={`\\lambda`} /> with the...
+          </TYU.HeaderMC>
+          <TYU.Explanation>
+            <p>
+              Remember that when selecting which model we want to use, we have
+              to use the validation set and not the training or test set. Recall
+              that if we use the training set to decide, we will favor models
+              that are overfit since they can have the potential to just
+              memorize the answers for the training set rather than
+              generalizing. Using the test set to select the model ruins the
+              point of the test set as a stand-in for future data, so we need to
+              use the validation set. This eliminates all options except:
+            </p>
+            <ul>
+              <li>
+                Smallest <IM math={`RSS(\\hat{w})`} /> on the{" "}
+                <em>validation set</em>.
+              </li>
+              <li>
+                Smallest{" "}
+                <IM math={`RSS(\\hat{w}) + \\lambda||\\hat{w}_{1:D}||_2^2`} />{" "}
+                on the <em>validation set</em>.
+              </li>
+            </ul>
+
+            <p>
+              It's a bit subtle which one of these are correct. Recall that with
+              regularization, we are changing our quality metric so that our
+              learned predictor is the following.
+            </p>
+
+            <BM
+              math={`\\hat{w}_{ridge} = \\min_w RSS(w) + \\lambda||w_{1:D}||_2^2`}
+            />
+
+            <p>
+              However, our task here is to compare two predictors learned from
+              say <IM math={`\\lambda = 0.01`} /> and{" "}
+              <IM math={`\\lambda = 10`} /> to identify which ones we think will
+              perform the best in the future. Our estimate for this value is{" "}
+              <IM math={`RSS_{validation}(\\hat{w})`} />, so if we want to pick
+              the one that we think will do best in the future, we should choose
+              the one with the smallest RSS on the validation set.
+            </p>
+
+            <p>
+              An alternative way of thinking about this: for different settings
+              of <IM math={`\\lambda`} /> (e.g., <IM math={`\\lambda = 0.01`} />{" "}
+              and <IM math={`\\lambda = 10`} />
+              ), the values of{" "}
+              <IM math={`RSS(\\hat{w}) + \\lambda||\\hat{w}_{1:D}||_2^2`} /> are
+              incomparable. Because there is a <IM math={`\\lambda`} /> inside
+              the expression, the "unit" of the values might be different with
+              differing values of <IM math={`\\lambda`} />. Since comparing
+              these numbers mean different things, we can't gain an insight into
+              which predictor is "better" by comparing them. However,{" "}
+              <IM math={`RSS_{validation}(\\hat{w})`} /> is always the same unit
+              regardless of which <IM math={`\\lambda`} /> you use, so you can
+              compare different predictors with this value.
+            </p>
+
+            <p>
+              This highlights what we said last time that this process for
+              choosing <IM math={`\\lambda`} /> is exactly the same as we
+              outlined in {bookContents.linkTo("assessing_performance")}. In
+              that chapter, we said you always choose the model that has the
+              lowest validation error, which is the same here!
+            </p>
+
+            <p>
+              To be a bit more concrete, our hyperparameter tuning algorithm for
+              Ridge regression is as follows. Pay particular attention to how
+              this is similar to the process described earlier. What are the key
+              parts that are different due to the specifics of Ridge?
+            </p>
+
+            {/* Please God have a better way of formatting this */}
+            <pre>
+              <code>
+                for each <IM math={`\\lambda`} /> in the{" "}
+                <IM math={`\\lambda`} />s you want to try:
+                <br />
+                {"    "}Train a model using your ML algorithm (e.g., Gradient
+                Descent)
+                <br />
+                {"      "}
+                <IM
+                  math={`\\hat{w}_{ridge(\\lambda)} = \\argmin RSS_{train}(w) + \\lambda ||w_{1:D}||_2^2`}
+                />
+                <br />
+                {"    "}Compute validation error
+                <br />
+                {"      "}
+                <IM
+                  math={`validation\\_error = RSS_{val}(\\hat{w}_{ridge(\\lambda)})`}
+                />
+                <br />
+                {"    "}Track <IM math={`\\lambda`} /> with smallest{" "}
+                <IM math={`validation\\_error`} />
+                <br />
+                Return <IM math={`\\lambda^*`} /> and estimate of future error{" "}
+                <IM math={`RSS_{test}(\\hat{w}_{ridge(\\lambda^*)})`} />
+              </code>
+            </pre>
+          </TYU.Explanation>
+        </TYU>
       </section>
 
       <section>
